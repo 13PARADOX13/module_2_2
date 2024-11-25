d11546e88cf4fe093fb54bffd6b7645770c957c5
13PARADOX13 <wdg42ru@gmail.com>
2024-11-25 15:10:53 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,21 +1,9 @@
 first = int(input())
 second = int(input())
 third = int(input())
-if first and second and third is type(int()) and first == second and first == third:
+if first == second and first == third:
     print(3)
+elif first == second or first == third or second == third:
+    print(2)
 else:
-    print('Введите целое число: ')
-
-
-# elif first and second and third is type(int())\
-# and first == second and first != third\
-# or first == third and first != second\
-# or second == third and second != first:
-#     print(2)
-#
-# elif first and second and third is type(int())\
-# and first != second and third and second != first and third:
-#     print(0)
-#
-# else:
-#     print('Введите целое число: ')
+    print(0)
